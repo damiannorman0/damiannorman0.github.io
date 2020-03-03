2c902e23b31742fcda519a42a1d39c3859a9a10f
Damian Norman <damiannorman@gmail.com>
2020-03-03 12:02:34 -0500

    build

---
+++ precache-manifest.60f0f9a555fe742e944f89d06c02dfad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d21aeb85a9a7935f92df14cfa5a6057",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db5ba4de18ac76930561",
+    "url": "/static/css/main.2002144e.chunk.css"
+  },
+  {
+    "revision": "b032808fab59c24ad4a8",
+    "url": "/static/js/2.1d4afd2c.chunk.js"
+  },
+  {
+    "revision": "d891fcc30dfc707e593ab65e09f6fef9",
+    "url": "/static/js/2.1d4afd2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "db5ba4de18ac76930561",
+    "url": "/static/js/main.6cf3c9a5.chunk.js"
+  },
+  {
+    "revision": "fb16b25e40463689d628",
+    "url": "/static/js/runtime-main.2ff6c32e.js"
+  }
+]);
